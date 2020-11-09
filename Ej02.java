5349437ccae070dca794b844e3b406dfa6c8c2b1
Ruben <ruben.lobato4@educa.madrid.org>
2020-11-09 10:06:34 +0100

    conflicto resuelto

---
+++ Ej02.java
@@ -0,0 +1,9 @@
+package sesion01;
+
+public class Ej02 {
+    public static void main(String[] args) {
+        System.out.println("primera");
+        System.out.println("segunda");
+        System.out.println("tercera");
+    }
+}
