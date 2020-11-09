cf3db537d9af2d0b57f41f49c581963089e3876a
Ruben <ruben.lobato4@educa.madrid.org>
2020-11-09 10:53:23 +0100

    tercer cambio

--- Ej02.java
+++ Ej02.java
@@ -2,7 +2,7 @@ package sesion01;
 
 public class Ej02 {
     public static void main(String[] args) {
-        System.out.println("primera");
+        System.out.println("cambio en la misma linea");
         System.out.println("segunda");
         System.out.println("tercera");
     }
