b4fb764853a8f66300af267b3408ba777456c62c
Ruben <ruben.lobato4@educa.madrid.org>
2020-11-11 16:10:49 +0100

    segunda solución al conflicto

--- Ej02.java
+++ Ej02.java
@@ -2,7 +2,11 @@ package sesion01;
 
 public class Ej02 {
     public static void main(String[] args) {
+<<<<<<< HEAD
         System.out.println("cambio en la misma linea");
+=======
+        System.out.println("el mismo cambio");
+>>>>>>> 79ee556 (tercer cambio)
         System.out.println("segunda");
         System.out.println("tercera");
     }
