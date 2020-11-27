11ea5cd88f1dd22264ecdf95b55cc6fef2d86bd6
Ruben <ruben.lobato4@educa.madrid.org>
2020-11-27 18:07:41 +0100

    segundo cambio

--- Ej02.java
+++ Ej02.java
@@ -3,7 +3,7 @@ package sesion01;
 public class Ej02 {
     public static void main(String[] args) {
 <<<<<<< HEAD
-        System.out.println("cambio en la misma linea");
+        System.out.println("segundo cambio");
 =======
         System.out.println("el mismo cambio");
 >>>>>>> 79ee556 (tercer cambio)
