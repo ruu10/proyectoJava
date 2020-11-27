479f4a43cab835ef27953ce7c88e0fcb5fcad9a8
Ruben <ruben.lobato4@educa.madrid.org>
2020-11-27 18:44:34 +0100

    segundo cambio

--- Ej01.java
+++ Ej01.java
@@ -3,10 +3,14 @@ package sesion01;
 public class Ej01 {
     public static void main(String[] args) {
 <<<<<<< HEAD
+<<<<<<< HEAD
         System.out.println("Hola, un cambio");
 =======
         System.out.println("segundo cambio");
 >>>>>>> 2548f11 (segundo cambio)
+=======
+        System.out.println("primer cambio");
+>>>>>>> f5f2dd6 (primer cambio)
     }
 
 }
